c11bd14dfd3de2d89f50b98347382efd4e2965e6
miawsama <initial21@mail.com>
2024-11-17 06:31:33 +0700

    Removing redundant function
    Signed-off-by: miawsama <initial21@mail.com>

--- lib/lib.py
+++ lib/lib.py
@@ -6,44 +6,6 @@ import platform
 
 
 class Main:
-    async def ainput(prompt: str = "") -> str:
-        loop = asyncio.get_event_loop()
-        return await loop.run_in_executor(None, input, prompt)
-
-    async def getRandomString(self, length):
-        letters = string.ascii_lowercase
-        return "".join(random.choice(letters) for i in range(length))
-
-    async def getDomain(self, request, maildomain):
-        async with request.get(
-            f"https://api.{maildomain}/domains", params={"page": "1"}
-        ) as resp:
-            return await resp.json()
-
-    async def registerAccount(self, session, maildomain, domain, passw):
-        async with session.post(
-            f"https://api.{maildomain}/accounts",
-            json={
-                "address": f'{await self.getRandomString(15)}@{domain["domain"]}',
-                "password": passw,
-            },
-        ) as resp:
-            register = await resp.json()
-        email = register["address"]
-        async with session.post(
-            f"https://api.{maildomain}/token",
-            json={"address": email, "password": passw},
-        ) as resp:
-            respJson = await resp.json()
-            token = respJson["token"]
-        return email, token
-
-    async def switchDomain(maildomain, externaldomain):
-        print(
-            f"Mail domain {maildomain} is not currently not available. Switching to the domain {externaldomain}..."
-        )
-        return externaldomain
-
     async def getSettingsAndBlockIP(self):
         try:
             data = toml.load("settings.toml")
